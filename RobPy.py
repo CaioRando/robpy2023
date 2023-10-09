a4a9ab25a37bcc72b78f2d3b087e5ccd31206851
CaioRando <caiomarostica@outlook.com>
2023-10-09 14:54:28 -0300

    inicio parte 4

--- RobPy.py
+++ RobPy.py
@@ -237,7 +237,7 @@ def checa_matriz_rotacao(m3: np.ndarray, det_tol: float = 0.01) -> None:
     erro = np.abs(1-np.linalg.det(m3))
     if erro > det_tol:
         raise ValueError('Pelo valor do determinante, esta matriz não é de rotação')
-    pass
+
 
 
 def cria_operador4(m_rot_b_a: np.ndarray = np.eye(3), v_o_a: np.ndarray = np.zeros([3, 1]), det_tol: float = 0.01) \
@@ -250,8 +250,13 @@ def cria_operador4(m_rot_b_a: np.ndarray = np.eye(3), v_o_a: np.ndarray = np.zer
     :param det_tol:
     :return:
     """
-    
-    pass
+    checa_matriz_rotacao(m_rot_b_a, det_tol)
+    checa_vetor3(v_o_a)
+
+    T = np.append(m_rot_b_a, v_o_a, axis=1) # adiciona no eixo das colunas
+    T = np.append(T, np.asarray([[0,0,0,1]]), axis=0) # eixo das linhas
+
+    return T
 
 
 def constroi_vetor(v_b: np.ndarray,
@@ -267,7 +272,14 @@ def constroi_vetor(v_b: np.ndarray,
     :param det_tol: tolerância do determinante
     :return: vetor (3, 1) na base a
     """
-    pass
+    checa_vetor3(v_b)
+    T = cria_operador4(m_rot_b_a=m_rot_b_a, v_o_a=v_o_a, det_tol=det_tol)
+
+    v_b4 = cria_vetor4(v_b)
+
+    v_a4 = T @ v_b4
+
+    return v_a4[0:3][:]
 
 
 # Parte 4
@@ -285,7 +297,14 @@ def __distancia_entre_retas_np(po1: np.ndarray, vs1: np.ndarray, po2: np.ndarray
     :param vs2: Vetor orientação da reta 1
     :return: distância entre as retas (float, positivo ou nulo)
     """
-    pass
+    checa_vetor3(vs1)
+    checa_vetor3(vs2)
+    checa_vetor3(po1)
+    checa_vetor3(po2)
+    v1 = po1 - po2
+    v2 = produto_vetorial(vs1, vs2)
+    v2 = v2 / norma_vetor(v2)
+    return norma_vetor(proj_vetores(v1, v2))
 
 
 def __distancia_entre_retas_p(po1: np.ndarray, po2: np.ndarray, vs: np.ndarray) -> float:
@@ -299,7 +318,12 @@ def __distancia_entre_retas_p(po1: np.ndarray, po2: np.ndarray, vs: np.ndarray) 
     :param vs: Vetor direção de ambas as retas
     :return: distância entre as retas (float, não negativo)
     """
-    pass
+    checa_vetor3(po1)
+    checa_vetor3(po2)
+    checa_vetor3(vs)
+    
+    v1 = po1 - po2
+    return norma_vetor(v1 - proj_vetores(v1, vs))
 
 
 def distancia_entre_retas(po1: np.ndarray, vs1: np.ndarray, po2: np.ndarray, vs2: np.ndarray, angtol=1e-3) -> float:
@@ -313,7 +337,16 @@ def distancia_entre_retas(po1: np.ndarray, vs1: np.ndarray, po2: np.ndarray, vs2
     :param angtol: Tolerância de ângulo entre as retas para decidir se são paralelas
     :return: Distância entre as retas (float, positivo ou nulo)
     """
-    pass
+
+    if angtol < 0:
+        raise ValueError('A tolerância angular deve ser um valor não negativo')
+
+    ang = np.abs(ang_vetores(vs1, vs2))
+    if ang < angtol or np.abs(ang-np.pi) < angtol:
+        return __distancia_entre_retas_p(po1, po2, vs1)
+    else:
+        return __distancia_entre_retas_np(po1, vs1, po2, vs2)
+
 
 
 def __eixo_reta_12_np(po1: np.ndarray, vs1: np.ndarray, po2: np.ndarray, vs2: np.ndarray) -> float:
--- testes.py
+++ testes.py
@@ -1,3 +1,10 @@
+import numpy as np
 import RobPy as rp
 
-help(rp.matriz_rotacao_x)
+M = rp.matriz_rotacao_x(43*np.pi/100)
+r = rp.cria_vetor3([2, 3, 1])
+
+
+T = rp.cria_operador4(M, r)
+
+print(T)
